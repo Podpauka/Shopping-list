c597afa001bf99b668fe6a2bc489a2a5d129c1ff
Podpauka <podpauka@gmail.com>
2021-06-15 18:09:04 +0200

    added class for database - the end

--- shopping_list/main.py
+++ shopping_list/main.py
@@ -2,25 +2,55 @@ import inspect
 import sqlite3
 
 
-def create_database(connection):
-    connection.execute('''CREATE TABLE List (
-                      id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name VARCHAR,
-                    amount INTEGER
-                    )''')
-    connection.commit()
+class ShoppingListDAO:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def create_database(self):
+        self.connection.execute('''CREATE TABLE List (
+                          id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name VARCHAR,
+                        amount INTEGER
+                        )''')
+        self.connection.commit()
+
+    def get_list(self):
+        response = self.connection.execute('''SELECT *
+                            FROM List
+                            ''')
+        all_items = response.fetchall()
+        return all_items
+
+    def insert_item(self, item, amount):
+        self.connection.execute(f'INSERT INTO List (name, amount) VALUES ("{item}", {amount})')
+        self.connection.commit()
+
+    def edit_item(self, edit_name, edit_amount, user_id_to_database_id, item_id):
+        self.connection.execute(
+            f'''
+            UPDATE List
+            SET name = "{edit_name}", amount = "{edit_amount}"
+            WHERE id = "{user_id_to_database_id[item_id]}"
+            '''
+        )
+        self.connection.commit()
+
+    def delete_item(self, user_id_to_database_id, item_id):
+        self.connection.execute(
+            f"""
+            DELETE from List
+            WHERE id = "{user_id_to_database_id[item_id]}"
+            """
+        )
+        self.connection.commit()
+
+    def clear_list(self):
+        self.connection.execute("DELETE FROM List")
+        self.connection.commit()
 
 
-def get_list(connection):
-    response = connection.execute('''SELECT *
-                        FROM List
-                        ''')
-    all_items = response.fetchall()
-    return all_items
-
-
-def show_list(connection, update_shopping_list):
-    shopping_list = get_list(connection)
+def show_list(shopping_list_dao: ShoppingListDAO, update_shopping_list):
+    shopping_list = shopping_list_dao.get_list()
     user_id_to_database_id = {index + 1: db_id for index, (db_id, _, _) in enumerate(shopping_list)}
     update_shopping_list(user_id_to_database_id)
     if shopping_list:
@@ -30,56 +60,39 @@ def show_list(connection, update_shopping_list):
         print("Lista jest pusta")
 
 
-def insert_item(item, amount, connection):
-    connection.execute(f'INSERT INTO List (name, amount) VALUES ("{item}", {amount})')
-    connection.commit()
-
-
-def add_item(connection, update_shopping_ids):
+def add_item(shopping_list_dao: ShoppingListDAO, update_shopping_ids):
     item = input("Wpisz nazwę produktu: ")
     amount = int(input("Podaj ilość"))
-    insert_item(item, amount, connection)
+    shopping_list_dao.insert_item(item, amount)
     update_shopping_ids()
     print(f"Dodano pozycję {item} do listy zakupów.")
 
 
-def edit_item(connection, user_id_to_database_id):
+def edit_item(shopping_list_dao: ShoppingListDAO, user_id_to_database_id):
     item_id = int(input("Podaj numer produktu, który chcesz edytować: "))
     edit_name = input("Wprowadź nową nazwę: ")
     edit_amount = int(input("Wprowadź nową ilość: "))
-    connection.execute(
-        f'''
-        UPDATE List
-        SET name = "{edit_name}", amount = "{edit_amount}"
-        WHERE id = "{user_id_to_database_id[item_id]}"
-        '''
-    )
-    connection.commit()
+    shopping_list_dao.edit_item(edit_name, edit_amount, user_id_to_database_id, item_id)
     print("Element został pomyślnie edytowany")
 
 
-def delete_item(connection, user_id_to_database_id, update_shopping_ids):
+def delete_item(shopping_list_dao: ShoppingListDAO, user_id_to_database_id, update_shopping_ids):
     item_id = int(input("Podaj numer produktu, który chcesz usunąć: "))
-    connection.execute(
-        f"""
-        DELETE from List
-        WHERE id = "{user_id_to_database_id[item_id]}"
-        """
-    )
-    connection.commit()
+    shopping_list_dao.delete_item(user_id_to_database_id, item_id)
     update_shopping_ids()
     print("Element został usunięty z listy.")
 
 
-def clear_list(connection, update_shopping_ids):
-    connection.execute("DELETE FROM List")
-    connection.commit()
+def clear_list(shopping_list_dao, update_shopping_ids):
+    shopping_list_dao.clear_list()
     update_shopping_ids()
+    print("Lista została wyczyszczona")
 
 
 def main():
     connection = sqlite3.connect('data.s3db')
-    create_database(connection)
+    shopping_list_dao = ShoppingListDAO(connection)
+    # shopping_list_dao.create_database()
     print("Witaj!")
     user_id_to_database_id = {}
 
@@ -106,15 +119,15 @@ def main():
             '''
         )))
         if option_choice == 1:
-            show_list(connection, set_shopping_ids)
+            show_list(shopping_list_dao, set_shopping_ids)
         elif option_choice == 2:
-            add_item(connection, update_shopping_ids)
+            add_item(shopping_list_dao, update_shopping_ids)
         elif option_choice == 3:
-            edit_item(connection, user_id_to_database_id)
+            edit_item(shopping_list_dao, user_id_to_database_id)
         elif option_choice == 4:
-            delete_item(connection, user_id_to_database_id, update_shopping_ids)
+            delete_item(shopping_list_dao, user_id_to_database_id, update_shopping_ids)
         elif option_choice == 5:
-            clear_list(connection, update_shopping_ids)
+            clear_list(shopping_list_dao, update_shopping_ids)
 
 
 main()
